03d9ea475e5713a90fb9459342aadd4fa127e34d
Flavio Souza <flavio.souza@calindra.com.br>
2020-12-30 11:27:40 -0300

    Adicionando lógica estatica

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
+const task = [
+  {
+    title: "Task 1",
+    category: "Finace",
+    emoji: "💰",
+    isCheck: false,
+  },
+  {
+    title: "Task 2",
+    category: "Finace",
+    emoji: "💰",
+    isCheck: true,
+  },
+  {
+    title: "Task 3",
+    category: "Finace",
+    emoji: "💰",
+    isCheck: false,
+  },
+  {
+    title: "Task 4",
+    category: "Finace",
+    emoji: "💰",
+    isCheck: true,
+  },
+];
+
+const incomplete = task.filter((t) => t.isCheck === false);
+const complete = task.filter((t) => t.isCheck === true);
+
 function App() {
   return (
     <>
-      <p>To do app</p>
+      <h2>Incomplete</h2>
+      {incomplete.map((item, index) => (
+        <p key={index}>
+          {item.title}
+          <br />
+          {item.emoji} {item.category}
+        </p>
+      ))}
+
+      <h2>Completed</h2>
+      {complete.map((item, index) => (
+        <p key={index}>
+          {item.title}
+          <br />
+          {item.emoji} {item.category}
+        </p>
+      ))}
     </>
   );
 }
