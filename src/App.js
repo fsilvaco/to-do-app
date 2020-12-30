e63a351c76b6223e337bf93519e82b13f14efaa4
Flavio Souza <flavio.souza@calindra.com.br>
2020-12-30 14:11:16 -0300

    Adicionando task via input com Hooks

--- src/App.js
+++ src/App.js
@@ -1,52 +1,26 @@
-const task = [
-  {
-    title: "Task 1",
-    category: "Finace",
-    emoji: "💰",
-    isCheck: false,
-  },
-  {
-    title: "Task 2",
-    category: "Finace",
-    emoji: "💰",
-    isCheck: true,
-  },
-  {
-    title: "Task 3",
-    category: "Finace",
-    emoji: "💰",
-    isCheck: false,
-  },
-  {
-    title: "Task 4",
-    category: "Finace",
-    emoji: "💰",
-    isCheck: true,
-  },
-];
-
-const incomplete = task.filter((t) => t.isCheck === false);
-const complete = task.filter((t) => t.isCheck === true);
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [task, setTask] = useState([]);
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTask([...task, input]);
+    setInput("");
+  };
+
   return (
     <>
-      <h2>Incomplete</h2>
-      {incomplete.map((item, index) => (
-        <p key={index}>
-          {item.title}
-          <br />
-          {item.emoji} {item.category}
-        </p>
-      ))}
-
-      <h2>Completed</h2>
-      {complete.map((item, index) => (
-        <p key={index}>
-          {item.title}
-          <br />
-          {item.emoji} {item.category}
-        </p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={input} onChange={handleChange}></input>
+        <button>Add task</button>
+      </form>
+      {task.map((item, index) => (
+        <p key={index}>{item}</p>
       ))}
     </>
   );
